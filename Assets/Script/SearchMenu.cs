445439f9268af1dd48b31e3c392dfd724a992718
Vallasc <giacomovallorani@protonmail.com>
2022-11-23 16:47:43 +0100

    Inital commit

---
+++ Assets/Script/SearchMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SearchMenu : MonoBehaviour
+{
+    public GameObject Element;
+    public GameObject ContentHolder;
+    public GameObject SearchBar;
+    public int totalElement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       //totalElement = ContentHolder.transform.childCount;
+       // Element = new GameObject(totalElement);
+       // for()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
